cc2da9d8b06e83aafe14f64a68fa0d1800dee22f
Tetiana <taniatop03@gmail.com>
2026-04-28 14:21:38 +0200

    task 5

---
+++ js/task-5.js
@@ -0,0 +1,14 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const colorButton = document.querySelector('button.change-color');
+const colorOutput = document.querySelector('span.color');
+
+colorButton.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorOutput.textContent = newColor;
+});
